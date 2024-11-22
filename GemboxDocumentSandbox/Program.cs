142bcaa176863a01854b24ed2636ff6e0a8462f4
sengel <sengel@plansoft.de>
2024-11-22 13:44:32 +0100

    replace Bookmark1

--- GemboxDocumentSandbox/Program.cs
+++ GemboxDocumentSandbox/Program.cs
@@ -3,7 +3,7 @@
 ComponentInfo.SetLicense("FREE-LIMITED-KEY");
 
 DocumentModel document = DocumentModel.Load("headerExample.docx");
-// document.Bookmarks["Bookmark1"].GetContent(false).LoadText("Bookmark1 replaced by this.");
+document.Bookmarks["Bookmark1"].GetContent(false).LoadText("Bookmark1 replaced by this.");
 document.Bookmarks["Bookmark2"].GetContent(false).LoadText("Bookmark2 replaced by this.");
 
 document.Save("test.pdf");
