a0ef9fe350cf28320f1ed4a89b3c4f38d9037e17
sengel <sengel@plansoft.de>
2024-11-22 13:43:10 +0100

    initial commit

---
+++ GemboxDocumentSandbox/Program.cs
@@ -0,0 +1,9 @@
+﻿using GemBox.Document;
+
+ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+
+DocumentModel document = DocumentModel.Load("headerExample.docx");
+// document.Bookmarks["Bookmark1"].GetContent(false).LoadText("Bookmark1 replaced by this.");
+document.Bookmarks["Bookmark2"].GetContent(false).LoadText("Bookmark2 replaced by this.");
+
+document.Save("test.pdf");
